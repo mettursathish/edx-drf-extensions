6ef597b869405316ab1dd4c411193874f0e49b61
SathishKumar Eswaran <seswar@microsoft.com>
2018-05-25 14:58:58 -0700

    Updated the class name and added waffle switch.

--- edx_rest_framework_extensions/permissions.py
+++ edx_rest_framework_extensions/permissions.py
@@ -1,8 +1,8 @@
 """ Permission classes. """
 from rest_framework.permissions import BasePermission
 from provider.oauth2.models import AccessToken as DOPAccessToken
-from edx_rest_framework_extensions.utils import jwt_decode_handler
-from oauth2_provider.ext.rest_framework.permissions import TokenHasScope
+from edx_rest_framework_extensions.utils import jwt_decode_handler, is_token_version_incompatable
+from openedx.core.djangoapps.oauth_dispatch.utils import is_oauth_scope_enforcement_enabled
 
 
 class IsSuperuser(BasePermission):
@@ -12,11 +12,10 @@ class IsSuperuser(BasePermission):
         return request.user and request.user.is_superuser
 
 
-class JWTRestrictedApplicationPermission(BasePermission):
+class HasScopedToken(BasePermission):
     """
     The request is authenticated and has the required scopes and organization filters
     """
-
     def _token_filters(self, decoded_token):
         # get filters list from jwt token and return dict
         if 'filters' in decoded_token:
@@ -34,25 +33,27 @@ class JWTRestrictedApplicationPermission(BasePermission):
         """
         Implement the business logic discussed above
         """
+        if is_oauth_scope_enforcement_enabled():
+            token = request.auth
+            decoded_token = jwt_decode_handler(token)
+            # check to see if token is a DOP token
+            # if so this represents a client which is implicitly trusted
+            # (since it is an internal Open edX application)
+            if isinstance(token, DOPAccessToken):
+                return True
 
-        token = request.auth
-        decoded_token = jwt_decode_handler(token)
-        # check to see if token is a DOP token
-        # if so this represents a client which is implicitly trusted
-        # (since it is an internal Open edX application)
-        if isinstance(token, DOPAccessToken):
-            return True
-
-        if not float(decoded_token['version']) <= 1.0:
-            return False
-
-        if hasattr(view, 'required_scopes'):
-            if not getattr(view, 'required_scopes')[0] in decoded_token['scopes']:
+            if is_token_version_incompatable(decoded_token['version']):
                 return False
 
-        has_permission = super(JWTRestrictedApplicationPermission, self).has_permission(request, view)
+            if hasattr(view, 'required_scopes'):
+                if not getattr(view, 'required_scopes')[0] in decoded_token['scopes']:
+                    return False
 
-        if has_permission:
-            setattr(request, 'oauth_scopes_filters', self._token_filters(decoded_token))
+            has_permission = super(HasScopedToken, self).has_permission(request, view)
 
-        return has_permission
+            if has_permission:
+                setattr(request, 'oauth_scopes_filters', self._token_filters(decoded_token))
+
+            return has_permission
+        else:
+            return True
